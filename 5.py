d2bad3bdfabed8d23a9d76f46272ef751cc14b70
mrtajik <satymov@outlook.com>
2020-07-26 18:42:51 -0400

    tuples

---
+++ 5.py
@@ -0,0 +1,29 @@
+"""
+Advance data type - Lists,tuple,dict,set
+"""
+#tuple non-changeable list
+"""
+ a=()
+ type(a)
+ <class 'tuple'>
+ c=(1,) or without () c=1,2,3,4,
+ type(c)
+ <class 'tuple'>
+ 
+ 
+ student=("Marry",18,"history",3.5)
+ type(student)
+ <class 'tuple'>
+ 
+ tuple unpacking
+ student=("Marry",18,"history",3.5)
+ name,age,subject,gpa =student
+ name
+ 'Marry'
+ age
+ 18
+ subject
+ 'history'
+ gpa
+ 3.5
+ """
