701251102c3b321fbad11406869b72eba5fc6026
mrtajik <satymov@outlook.com>
2020-07-26 19:13:16 -0400

    sets

---
+++ 6.py
@@ -0,0 +1,28 @@
+"""
+Advance data type - Lists,tuple,dict,set
+"""
+#sets - to store  immutable object is an object whose state 
+#cannot be modified after it is created.
+"""
+set()
+set()
+names={"max","nina","poly"}
+type(names)
+<class 'set'>
+"""
+#add/remove/update sets
+""""
+names.add("rosy")
+names
+{'nina', 'max', 'poly', 'rosy'}
+
+names.discard("nina")
+names
+{'max', 'poly', 'rosy'}
+
+num={1,2,4}
+names.update(num)
+names
+{1, 'max', 'poly', 2, 4, 'rosy'}
+"""
+#Combining,Comparing and Contrasting
