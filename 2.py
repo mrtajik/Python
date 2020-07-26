86be4cd20a6fc354e4333088fe43598305b11336
mrtajik <satymov@outlook.com>
2020-07-26 15:59:42 -0400

    Data Types

--- 2.py
+++ 2.py
@@ -1,7 +1,23 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-Created on Sun Jul 26 12:59:52 2020
+Introduction to Python 3
+variables int = whole numbers positive and negative
+          float= 3.0
+          complex=45j 
 
-@author: mubs
+Strings   in ' ' or " " (double better)
+          concatenate "Max"+" I "='Max I '  
+long_string="" "   text " ""(triple)
+f string   example:
+          name="Max"
+          f"Hello, {name}"
+    out:  'Hello, Max'
+
+
+dir,type, NULL-None
+help(name.upper)- will show what it does and so on
+
 """
+name="Max"
+print(type(name))
+print(dir(name))
+
