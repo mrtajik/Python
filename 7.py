97294e08e9b7a7d22a34a43bbcbcd06d5efc4897
mrtajik <satymov@outlook.com>
2020-07-29 15:10:26 -0400

    dictionary

---
+++ 7.py
@@ -0,0 +1,40 @@
+"""
+Advance data type - Lists,tuple,dict,set
+"""
+#dictionary
+"""
+ dict({})
+{}
+type({})
+<class 'dict'>
+
+{1:"test"}
+{1: 'test'}
+where 1: is the immutable key, test is the value
+accessing to data through key:
+ num[1]
+'one'
+
+num.get(1)
+'one'
+
+to add
+num["four"]=4
+nnum
+{1: 'one', 2: 'two', 3: 'three', 'four': 4}
+
+search
+1 in num
+True
+
+combain two dict.
+.update
+
+.keys-to call keys
+.values - to call values
+
+remove
+.pop
+
+
+"""
