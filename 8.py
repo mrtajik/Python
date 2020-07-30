7cfeff61e5050a9c8c8581aad2798901778ed6cd
mrtajik <satymov@outlook.com>
2020-07-30 10:56:04 -0400

    boolean logic

---
+++ 8.py
@@ -0,0 +1,14 @@
+"""
+Boolean logic
+"""
+#<=; >=; <; >; ==; !=; and; or;  not;
+x = 5
+y = 8
+a=False
+print("x == y:", x == y)
+print("x != y:", x != y)
+print("x < y:", x < y)
+print("x > y:", x > y)
+print("x <= y:", x <= y)
+print("x >= y:", x >= y)
+print("not a =", not a)
